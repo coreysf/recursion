ad7ff835dd3ffe217dcdb24a37bf411e9d177ece
Corey Roy <corey.j.roy@gmail.com>
2015-07-18 21:13:37 -0700

    First draft - 7 of 20 test cases pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,45 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+
+	if (typeof obj === 'string') {
+		return '"' + obj.toString() + '"';
+	}
+
+	if (typeof obj === 'boolean' || typeof obj === 'number') {
+		return obj.toString();
+	}
+
+	if (obj === null && typeof obj === 'object') {
+		return 'null';
+	}
+
+	if (Array.isArray(obj) && obj.length == 0) {
+		return '[]';
+	}
+
+	if (Array.isArray(obj)) {
+		arrString = '[';
+		for (var i = 0; i < obj.length - 1; i++) {
+			arrString += obj[i] + ', ';
+		}
+		arrString += obj[obj.length - 1] + ']';
+		return arrString;
+	}
+
+	if (typeof obj === 'object')  {
+		objString = '{ ';
+		for (var key in obj) {
+			objString += '"' + key + '": ';
+			if (typeof obj[key] === 'string') {
+				objString += '"';
+			} 
+			objString += obj[key];
+			if (typeof obj[key] === 'string') {
+				objString += '"';
+			}
+			objString += ', ';
+		}
+	}
 };
