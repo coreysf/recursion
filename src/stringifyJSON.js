3c9659327bfd1ca2c7b1fdb73327b75fe411df37
Corey Roy <corey.j.roy@gmail.com>
2015-07-20 15:49:05 -0700

    Pass all stringifyJSON tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -32,7 +32,7 @@ var stringifyJSON = function(obj) {
 		}
 	};
 
-	var arrStringFunc = function(arr) {
+	function arrStringFunc (arr) {
 			arrString = '';
 			for (var i = 0; i < arr.length - 1; i++) {
 				if (Array.isArray(arr[i])) {
@@ -43,6 +43,11 @@ var stringifyJSON = function(obj) {
 					arrString += arrStringFunc(arr[i]) + ',';
 					}
 				}
+
+				else if (typeof arr[i] === 'object' && !Array.isArray(arr[i])) {
+					arrString += objStringFunc(arr[i]) + ',';
+				}
+
 				else {
 					arrString += primValues(arr[i]) + ',';
 					console.log(arrString);
@@ -52,6 +57,11 @@ var stringifyJSON = function(obj) {
 			if (Array.isArray(arr[arr.length-1])) {
 				arrString += arrStringFunc(arr[arr.length-1]);
 			}
+
+			else if (typeof arr[arr.length-1] === 'object' && !Array.isArray(arr[arr.length-1])) {
+				arrString += objStringFunc(arr[arr.length-1]);
+			}
+
 			else {
 				arrString += primValues(arr[arr.length - 1]);
 				console.log(arrString);
@@ -75,30 +85,38 @@ var stringifyJSON = function(obj) {
 
 
 		
-	var objStringFunc = function(collection) {
+	function objStringFunc (collection) {
 		objString = '{';
 		for (var key in collection) {
-			if ((typeof collection[key] === 'object' && !Array.isArray(collection[key])) && collection[key] !== null) {
-				objString += '"' + key + '":';
-				objString += objStringFunc(collection[key]);
-			}
-
-			else if (Array.isArray(collection[key])) {
-				console.log(true);
-				objString += '"' + key + '":';
-				objString += arrStringFunc(collection[key]);
-			}
-
-			else {
-				objString += '"' + key + '":';
-				if (typeof collection[key] === 'string') {
-					objString += '"';
-				} 
-				objString += collection[key];
-				if (typeof collection[key] === 'string') {
-					objString += '"';
+			if (collection[key] !== undefined && typeof collection[key] !== 'function') {
+				if ((typeof collection[key] === 'object' && !Array.isArray(collection[key])) && collection[key] !== null) {
+					objString += '"' + key + '":';
+					objString += objStringFunc(collection[key]) + ',';
 				}
-				objString += ',';
+
+				else if (Array.isArray(collection[key]) && collection[key].length == 0) {
+					console.log(true);
+					objString += '"' + key + '":';
+					objString += '[],';
+				}
+
+				else if (Array.isArray(collection[key])) {
+					console.log(true);
+					objString += '"' + key + '":';
+					objString += arrStringFunc(collection[key]) + ',';
+				}
+
+				else {
+					objString += '"' + key + '":';
+					if (typeof collection[key] === 'string') {
+						objString += '"';
+					} 
+					objString += collection[key];
+					if (typeof collection[key] === 'string') {
+						objString += '"';
+					}
+					objString += ',';
+				}
 			}
 		}
 
